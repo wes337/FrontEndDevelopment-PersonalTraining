06cd23de6b7d347394ba5b94d5b6b912e220b4ce
wes337 <wesmoses@gmail.com>
2018-11-26 15:58:51 +0200

    can see customer schedules, fixed delete, working on adding trainings

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -2,36 +2,55 @@ import React, { Component } from 'react';
 import ReactTable from 'react-table';
 import Addcustomer from './Addcustomer.js';
 import 'react-table/react-table.css';
-import { Alert, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Alert, Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Input, Label } from 'reactstrap';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faCalendarAlt, faPlus } from '@fortawesome/free-solid-svg-icons';
 import '../App.css';
-import Addparticipants from './Addparticipants.js';
-import Customerschedule from './Customerschedule.js';
+import moment from 'moment';
 
-library.add(faTrash);
+library.add(faTrash, faCalendarAlt, faPlus);
 
 class Customerlist extends Component {
     constructor(params) {
         super(params);
         this.state = {
             customers: [],
+            customer: "",
+            selectedtraining: [],
+            trainings: [],
             modal: false,
+            modal2: false,
             visible: false,
+            deletevisible: false,
         };
         this.toggle = this.toggle.bind(this);
+        this.toggle2 = this.toggle2.bind(this);
         this.onDismiss = this.onDismiss.bind(this);
     }
 
-    toggle() {
+    componentDidMount() {
+        this.listCustomers();
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    toggle2() {this.toggle2 = this.toggle2.bind(this);
+        this.setState({
+          modal2: !this.state.modal2
+        });
+    }
+
+    toggle() {this.toggle = this.toggle.bind(this);
         this.setState({
           modal: !this.state.modal
         });
     }
 
     onDismiss() {
-        this.setState({ visible: false });
+        this.setState({ visible: false, deletevisible: false });
     }
 
     listCustomers() {
@@ -42,9 +61,30 @@ class Customerlist extends Component {
         })
     }
 
-    componentDidMount() {
-        this.listCustomers();
+    listTrainings = () => {
+        fetch('https://customerrest.herokuapp.com/api/trainings')
+        .then(response => response.json())
+        .then(responseData => {
+            this.setState({trainings: responseData.content})
+        })
     }
+
+    getTrainings = (link) => {
+        fetch(link)
+        .then(response => response.json())
+        .then(responseData => {
+            this.setState({
+                trainings: responseData.content
+            })
+        })
+        this.toggle();
+    }
+
+    setCustomer = (customer) => {
+        this.setState({ customer: customer });
+        this.listTrainings();
+        this.toggle2();
+    };
 
     filterMethod = (filter, row, column) => {
         const id = filter.pivotId || filter.id
@@ -66,15 +106,48 @@ class Customerlist extends Component {
         })
     }
 
+      // Save a new training
+    saveTraining = (trg) => {
+     //   const training = {
+     //   date: this.state.selectedtraining.date,
+     //   activity: this.state.selectedtraining.activity,
+     //   duration: this.state.selectedtraining.duration,
+     //   customer: this.state.customer
+     //   };
+       // fetch("https://customerrest.herokuapp.com/api/trainings" + this.state.selectedtraining.id, {
+      //  method: "POST",
+      //  headers: { "Content-Type": "application/json" },
+      //  body: JSON.stringify(training)
+      //  });
+        console.log(trg)
+        this.toggle2();
+    };
+
     deleteCustomer = (link) => {
         fetch(link, {method: 'DELETE'})
         .then(response => {
             this.listCustomers();
+            {this.setState({ deletevisible: true })}
         })
         this.toggle();
     }
 
     render() {
+        const individualTrainingColumns = [
+            {
+              Header: "Date and time",
+              accessor: "date",
+              Cell: ({ value }) => moment(value).format("MMM Do YYYY, h:mm a")
+            },
+            {
+              Header: "Duration (in minutes)",
+              accessor: "duration"
+            },
+            {
+              Header: "Activity",
+              accessor: "activity"
+            }
+        ];
         const columns = [{
             Header: 'Name',
             columns: [
@@ -114,18 +187,21 @@ class Customerlist extends Component {
             filterable: false,
             sortable: false,
             Cell: ({row, value}) => (
-                <div>
-                    <Addparticipants />
+                <div className="text-center">
+                    <Button outline color="primary" size="sm" onClick={() => this.setCustomer(value)}>
+                        <FontAwesomeIcon icon="plus" />
+                    </Button> 
                 </div>
             )
         }, {
             Header: '',
-            accessor: '',
+            accessor: 'links.2.href',
             filterable: false,
             sortable: false,
             Cell: ({row, value}) => (
-                <div>
-                    <Customerschedule />               
+                <div className="text-center">
+                    <Button outline color="warning" size="sm" onClick={() => this.getTrainings(value)}><FontAwesomeIcon icon="calendar-alt" />
+                    </Button>             
                 </div>
             )            
         }, {
@@ -135,18 +211,18 @@ class Customerlist extends Component {
             sortable: false,
             Cell: ({row, value}) => (
                 <div className="text-center">
-                    <Button outline color="danger" size="sm" onClick={this.toggle}><FontAwesomeIcon icon="trash" /></Button>
-                    <Modal isOpen={this.state.modal} toggle={this.toggle} backdrop={false} centered>
-                    <ModalHeader toggle={this.toggle} className="bg-danger text-white">Delete Customer</ModalHeader>
-                        <ModalBody>
-                            Are you sure?
-                        </ModalBody>
-                        <ModalFooter>
-                            <Button color="danger" onClick={()=> this.deleteCustomer(value)}>Delete</Button>{' '}
-                            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                        </ModalFooter>
-                    </Modal>
+                    <Button outline color="danger" size="sm"
+                    onClick={() => {
+                    if (
+                        window.confirm(
+                        "Are you sure you want to delete " + row.firstname + " " + row.lastname + "?"
+                        )
+                    )
+                        this.deleteCustomer(value);
+                    }}
+                    ><FontAwesomeIcon icon="trash" /></Button>
                 </div>
+                
             )
         }]
         return (
@@ -161,7 +237,47 @@ class Customerlist extends Component {
                 />
                 <Alert color="success" isOpen={this.state.visible} toggle={this.onDismiss} className="fixed-bottom">
                     Customer Added Successfully!
+                </Alert>faPlus
+                <Alert color="success" isOpen={this.state.deletevisible} toggle={this.onDismiss} className="fixed-bottom">
+                    Customer Deleted Successfully!
                 </Alert>
+
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                <ModalHeader toggle={this.toggle}><h3>Individual training sessions</h3></ModalHeader>
+                <ModalBody>
+                    <ReactTable
+                        filtselectMultierable={false}
+                        resizable={false}
+                        defaultPageSize={10}
+                        data={this.state.trainings}
+                        columns={individualTrainingColumns}
+                        showPagination={false}
+                    />  
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                </ModalFooter>
+                </Modal>
+
+                <Modal isOpen={this.state.modal2} toggle={this.toggle2} className={this.props.className}>
+                <ModalHeader toggle={this.toggle2}><h3>Add Training</h3></ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Label for="exampleSelectMulti">Select</Label>
+                            <Input type="select" value={this.state.selectedtraining} id="selectedtraining" name="selectedtraining" onChange={this.handleChange}>
+                            {this.state.trainings.map((training, index) =>
+                                <option key={index}>{training.activity} ({moment(training.date).format("DD MMM YYYY")})</option>
+                            )}                                    
+                            </Input>
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={()=>this.saveTraining(this.state.selectedtraining)}>Add</Button>{' '}
+                    <Button color="secondary" onClick={this.toggle2}>Cancel</Button>
+                </ModalFooter>
+                </Modal>
             </div>
         );
     }
