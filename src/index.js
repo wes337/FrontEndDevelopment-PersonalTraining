8a596b418b388627294fc788d38f5efaf6407526
Wes Moses <wesmoses@gmail.com>
2018-12-06 17:39:46 +0200

    added firebase authentication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 import './style.css';
 import './docs.css';
+import './custom.css';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+
+
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>,
+    document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
