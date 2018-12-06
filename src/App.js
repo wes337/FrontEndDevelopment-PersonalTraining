ba4a6d3ee9814829418fdca42a106cac442e8df7
Wes Moses <wesmoses@gmail.com>
2018-12-06 14:59:15 +0200

    removed console logging search state

--- src/App.js
+++ src/App.js
@@ -81,7 +81,6 @@ class App extends Component {
 
   handleChange = (event) => {
     this.setState({[event.target.name]: event.target.value});
-    console.log(this.state.search)
   }
 
   render() {
